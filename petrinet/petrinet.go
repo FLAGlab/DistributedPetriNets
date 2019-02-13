4c5f6ff24fc6a4b0bbd42104b39867ed95436945
jssosa10 <js.sosa10@uniandes.edu.co>
2019-02-13 15:11:13 -0500

    inhibitor arcs

--- petrinet/petrinet.go
+++ petrinet/petrinet.go
@@ -2,15 +2,17 @@
 P => places
 T => transitions
 F subset of (P X T) U (T X P) => arcs
-W: F -> {1,2,3,...} => weights
+F0 subset of (P X T) => inhibitor arcs
+W: F -> {1,2,3,...} => weights of arcs
+W0: F0 -> {1,2,3,...} => weights of inhibitor arcs
 M_0: P -> {0, 1, 2, 3, ...} => Initial marking
 {
   P: Number => 1...P places
   T: Number => 1...T transitions
   Fpt: [{p: Number, t: Number, w: Number}] => Arcs from P to T list
   Ftp: [{p: Number, t: Number, w: Number}] => Arcs from T to P list
+  Inhibitors: [{p: Number, t: Number, w: Number}] => Inhibitor Arcs from P to T list
   M: [{p: Number, m: Number}] => Initial marking list,
-  I: Number => max number of transitions to fire
 }
 
 eg.
@@ -18,9 +20,9 @@ eg.
   P: 4,
   T: 2,
   Fpt: [{p: 1, t: 1, w: 1}, {p: 2, t: 2, w: 1}, {p: 3, t: 2, w: 1}],
-  Ftp: [{t: 1, p: 2, w: 1}, {t: 1, p: 3, w: 1}, {t: 2, p: 4, w: 1}, {t: 2, p: 1, w: 1}],
+  Ftp: [{t: 1, p: 2, w: 1}, {t: 1, p: 3, w: 1}, {t: 2, p: 4, w: 1}],
+  Inhibitors: [{t: 2, p: 4, w: 1 }],
   M: [{p: 1, m: 1}, {p: 3, m: 2}, {p: 4, m: 1}],
-  I: 10
 }
 */
 package main
@@ -44,6 +46,7 @@ type PetriNet struct {
   t int
   tIn map[int][]Arc
   tOut map[int][]Arc
+  inhibitors map[int][]Arc
   m map[int]int
   //it int
 }
@@ -51,9 +54,10 @@ func (pn PetriNet) String() string {
 	return fmt.Sprintf("{\n\tp: %v, \n\tt: %v, \n\ttIn: %v, \n\ttOut: %v, \n\tm: %v\n}", pn.p, pn.t, pn.tIn, pn.tOut, pn.m)
 }
 
-func initPetriNet(p, t int, fpt, ftp []Arc, m0 map[int]int) PetriNet {
+func initPetriNet(p, t int, fpt, ftp, inhi []Arc, m0 map[int]int) PetriNet {
   tIn  := make(map[int][]Arc)
   tOut := make(map[int][]Arc)
+  inhibitors := make(map[int][]Arc)
 
   for _, arc := range fpt {
     tIn[arc.t] = append(tIn[arc.t], arc)
@@ -61,7 +65,10 @@ func initPetriNet(p, t int, fpt, ftp []Arc, m0 map[int]int) PetriNet {
   for _, arc := range ftp {
     tOut[arc.t] = append(tOut[arc.t], arc)
   }
-  return PetriNet{p, t, tIn, tOut, m0}
+  for _, arc :=range inhi {
+    inhibitors[arc.t] = append(inhibitors[arc.t],arc)
+  }
+  return PetriNet{p, t, tIn, tOut, inhibitors, m0}
 }
 
 func (pn *PetriNet) Run() {
@@ -89,6 +96,10 @@ func (pn *PetriNet) canTransition(currT int) bool {
   for _, value := range inArcs {
     ans = ans && pn.m[value.p] >= value.w
   }
+  inhibArcs := pn.inhibitors[currT]
+  for _, value := range inhibArcs {
+    ans = ans && pn.m[value.p] < value.w
+  }
   return ans
 }
 
@@ -107,10 +118,11 @@ func main() {
   fpt := []Arc{Arc{1, 1, 1}, Arc{2, 2, 1}, Arc{3, 2, 1}}
   ftp := []Arc{Arc{t: 1, p: 2, w: 1}, Arc{t: 1, p: 3, w: 1}, Arc{t: 2, p: 4, w: 1}}
   m := make(map[int]int)
+  inhi := []Arc{Arc{t: 2, p: 4, w:1}}
   m[1] = 1
   m[3] = 2
   m[4] = 1
-  p := initPetriNet(4, 2, fpt, ftp, m)
+  p := initPetriNet(4, 2, fpt, ftp, inhi, m)
   fmt.Printf("%v", p)
   p.Run()
 }
