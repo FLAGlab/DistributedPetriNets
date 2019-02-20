ce161439be868fa48c0ccd208ac7405ea7f8b052
jdfandino10 <jd.fandino10@uniandes.edu.co>
2019-02-20 18:35:39 -0500

    transition

--- petrinet/transtion.go
+++ petrinet/transtion.go
@@ -1,8 +1,29 @@
 package petrinet
 
-
-
-type Transition struct { 
+type transition struct {
 	id int
 	priority int
+	inArcs []Arc
+	outArcs []Arc
+	inhibitorArcs []Arc
+}
+
+func (t *transition) canFire() bool {
+	ans := true
+  for _, value := range t.inArcs {
+		ans = ans && value.place.marks >= value.weight
+  }
+  for _, value := range t.inhibitorArcs {
+    ans = ans && value.place.marks < value.weight
+  }
+  return ans
+}
+
+func (t *transition) fire() {
+	for _, currArc := range t.inArcs {
+    currArc.place.marks -= currArc.weight
+  }
+  for _, currArc := range t.outArcs {
+    currArc.place.marks += currArc.weight
+  }
 }
