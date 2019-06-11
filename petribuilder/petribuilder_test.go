499943d725bff8ac13b97c1a5143aeaff2a1d80d
jdfandino10 <jd.fandino10@uniandes.edu.co>
2019-06-11 00:05:41 -0500

    completed tests

--- petribuilder/petribuilder_test.go
+++ petribuilder/petribuilder_test.go
@@ -274,7 +274,6 @@ func TestGetUniversalPetriNet(t *testing.T) {
     if foundIndex == -1 {
       t.Errorf("Didn't expect universal petri net to have internal transition %v. Missing expected: %v", tr, expectedInternal)
     } else {
-      t.Logf("Found %v at index %v (Value: %v)", *tr, foundIndex, expectedInternal[foundIndex])
       removeNoOrder(expectedInternal, foundIndex)
     }
   }
@@ -306,36 +305,208 @@ func TestGetUniversalPetriNet(t *testing.T) {
   if !reflect.DeepEqual(clearTr, expectedClear) {
     t.Errorf("Expected universal petri net to have a clear transition %v but was %v", expectedClear, clearTr)
   }
-  // TODO: check that each set of rmt transitions is as expected
 }
 
-func TestUpdatePetriNetWithCDR(t *testing.T) {
+func helperUpdatePetriNetWithCDRfunc(pn *petrinet.PetriNet, expectedInternal map[int]petrinet.RemoteTransition, t *testing.T) {
+  // make sure every transition can fire
+  pn.FireTransitionByID(REQ_TR)
+  pn.FireTransitionByID(REQ_TR)
+  pn.FireTransitionByID(ACT_TR)
+  pn.FireTransitionByID(REQ_NOT_TR)
+
+  _, rmtInternalRef := pn.GetTransitionOptionsByPriority(INTERNAL_TRANSITION_PRIORITY)
+  _, rmtExternalRef := pn.GetTransitionOptionsByPriority(EXTERNAL_TRANSITION_PRIORITY)
+  _, rmtClearRef := pn.GetTransitionOptionsByPriority(CLEAR_TRANSITION_PRIORITY)
+  rmtInternal := make(map[int]petrinet.RemoteTransition)
+  for id, rmt := range rmtInternalRef {
+    rmtInternal[id] = *rmt
+  }
+  rmtExternal := make(map[int]petrinet.RemoteTransition)
+  for id, rmt := range rmtExternalRef {
+    rmtExternal[id] = *rmt
+  }
+  rmtClear := make(map[int]petrinet.RemoteTransition)
+  for id, rmt := range rmtClearRef {
+    rmtClear[id] = *rmt
+  }
+  t.Logf("Rmt expected internal: %v", expectedInternal)
+  t.Logf("Rmt internal: %v", rmtInternal)
+  t.Logf("Rmt external: %v", rmtExternal)
+  t.Logf("Rmt clear: %v", rmtClear)
+  if len(rmtExternal) != 0 {
+    t.Errorf("Expected no remote external transitions on context %v but had %v", pn.Context, rmtExternal)
+  }
+  if len(rmtClear) != 0 {
+    t.Errorf("Expected no remote clear transitions on context %v but had %v", pn.Context, rmtExternal)
+  }
+  if len(rmtInternal) != len(expectedInternal) {
+    t.Errorf("Expected %v remote internal transitions but were %v", len(expectedInternal), len(rmtInternal))
+  }
+  for id, expectedTr := range expectedInternal {
+    t.Logf("Current transition id: %v", id)
+    t.Logf("Map value of that id in expected internal: %v", expectedInternal[id])
+    t.Logf("Map value of that id in existing internal: %v", rmtInternal[id])
+    existing, exists := rmtInternal[id]
+    if !exists {
+      t.Errorf("Expect universal petri net to have internal transition %v but didn't exist", expectedTr)
+    } else if !reflect.DeepEqual(expectedTr, existing) {
+      t.Errorf("Expect universal petri net to have internal transition %v but was %v", expectedTr, existing)
+    }
+  }
+}
+
+func TestUpdatePetriNetWithCDRExclusion(t *testing.T) {
   // a and b exclusion
   a := CreateContext("a")
   b := CreateContext("b")
+  UpdatePetriNetWithCDR(a, TEST_CDR)
+  UpdatePetriNetWithCDR(b, TEST_CDR)
+  expected := map[int]petrinet.RemoteTransition {
+    ACT_TR: petrinet.RemoteTransition{
+      ID: ACT_TR,
+      InArcs: []petrinet.RemoteArc{},
+      OutArcs: []petrinet.RemoteArc{},
+      InhibitorArcs: []petrinet.RemoteArc{
+        petrinet.RemoteArc{PlaceID: CTX_PLACE,Context: "b",Address: "",Weight: 1,Marks: 0},
+      },
+    },
+  }
+  helperUpdatePetriNetWithCDRfunc(a, expected, t)
+
+  expected = map[int]petrinet.RemoteTransition {
+    ACT_TR: petrinet.RemoteTransition{
+      ID: ACT_TR,
+      InArcs: []petrinet.RemoteArc{},
+      OutArcs: []petrinet.RemoteArc{},
+      InhibitorArcs: []petrinet.RemoteArc{
+        petrinet.RemoteArc{PlaceID: CTX_PLACE,Context: "a",Address: "",Weight: 1,Marks: 0},
+      },
+    },
+  }
+  helperUpdatePetriNetWithCDRfunc(b, expected, t)
+}
+
+func TestUpdatePetriNetWithCDRCausality(t *testing.T) {
   // c and d causality
   c := CreateContext("c")
   d := CreateContext("d")
+  UpdatePetriNetWithCDR(c, TEST_CDR)
+  UpdatePetriNetWithCDR(d, TEST_CDR)
+  expected := map[int]petrinet.RemoteTransition {
+    ACT_TR: petrinet.RemoteTransition{
+      ID: ACT_TR,
+      InArcs: []petrinet.RemoteArc{},
+      OutArcs: []petrinet.RemoteArc{
+        petrinet.RemoteArc{PlaceID: PR_PLACE,Context: "d",Address: "",Weight: 1,Marks: 0},
+      },
+      InhibitorArcs: []petrinet.RemoteArc{},
+    },
+    DEACT_TR: petrinet.RemoteTransition{
+      ID: DEACT_TR,
+      InArcs: []petrinet.RemoteArc{
+        petrinet.RemoteArc{PlaceID: CTX_PLACE,Context: "d",Address: "",Weight: 1,Marks: 0},
+      },
+      OutArcs: []petrinet.RemoteArc{
+        petrinet.RemoteArc{PlaceID: PR_NOT_PLACE,Context: "d",Address: "",Weight: 1,Marks: 0},
+        petrinet.RemoteArc{PlaceID: CTX_PLACE,Context: "d",Address: "",Weight: 1,Marks: 0},
+      },
+      InhibitorArcs: []petrinet.RemoteArc{},
+    },
+  }
+  helperUpdatePetriNetWithCDRfunc(c, expected, t)
+
+  expected = map[int]petrinet.RemoteTransition {}
+  helperUpdatePetriNetWithCDRfunc(d, expected, t)
+}
+
+func TestUpdatePetriNetWithCDRImplication(t *testing.T) {
   // e and f implication
   e := CreateContext("e")
   f := CreateContext("f")
+  UpdatePetriNetWithCDR(e, TEST_CDR)
+  UpdatePetriNetWithCDR(f, TEST_CDR)
+
+  expected := map[int]petrinet.RemoteTransition {
+    ACT_TR: petrinet.RemoteTransition{
+      ID: ACT_TR,
+      InArcs: []petrinet.RemoteArc{},
+      OutArcs: []petrinet.RemoteArc{
+        petrinet.RemoteArc{PlaceID: PR_PLACE,Context: "f",Address: "",Weight: 1,Marks: 0},
+      },
+      InhibitorArcs: []petrinet.RemoteArc{},
+    },
+    DEACT_TR: petrinet.RemoteTransition{
+      ID: DEACT_TR,
+      InArcs: []petrinet.RemoteArc{},
+      OutArcs: []petrinet.RemoteArc{
+        petrinet.RemoteArc{PlaceID: PR_NOT_PLACE,Context: "f",Address: "",Weight: 1,Marks: 0},
+      },
+      InhibitorArcs: []petrinet.RemoteArc{},
+    },
+  }
+  helperUpdatePetriNetWithCDRfunc(e, expected, t)
+
+  expected = map[int]petrinet.RemoteTransition {}
+  helperUpdatePetriNetWithCDRfunc(f, expected, t)
+}
+
+func TestUpdatePetriNetWithCDRRequirement(t *testing.T) {
   // g and h requirement
   g := CreateContext("g")
   h := CreateContext("h")
+  UpdatePetriNetWithCDR(g, TEST_CDR)
+  UpdatePetriNetWithCDR(h, TEST_CDR)
+
+  expected := map[int]petrinet.RemoteTransition {
+    ACT_TR: petrinet.RemoteTransition{
+      ID: ACT_TR,
+      InArcs: []petrinet.RemoteArc{
+        petrinet.RemoteArc{PlaceID: CTX_PLACE,Context: "h",Address: "",Weight: 1,Marks: 0},
+      },
+      OutArcs: []petrinet.RemoteArc{
+        petrinet.RemoteArc{PlaceID: CTX_PLACE,Context: "h",Address: "",Weight: 1,Marks: 0},
+      },
+      InhibitorArcs: []petrinet.RemoteArc{},
+    },
+  }
+  helperUpdatePetriNetWithCDRfunc(g, expected, t)
+
+  expected = map[int]petrinet.RemoteTransition {}
+  helperUpdatePetriNetWithCDRfunc(h, expected, t)
+}
+
+func TestUpdatePetriNetWithCDRSuggestion(t *testing.T) {
   // i and j suggestion
   i := CreateContext("i")
   j := CreateContext("j")
-  UpdatePetriNetWithCDR(a, TEST_CDR)
-  UpdatePetriNetWithCDR(b, TEST_CDR)
-  UpdatePetriNetWithCDR(c, TEST_CDR)
-  UpdatePetriNetWithCDR(d, TEST_CDR)
-  UpdatePetriNetWithCDR(e, TEST_CDR)
-  UpdatePetriNetWithCDR(f, TEST_CDR)
-  UpdatePetriNetWithCDR(g, TEST_CDR)
-  UpdatePetriNetWithCDR(h, TEST_CDR)
   UpdatePetriNetWithCDR(i, TEST_CDR)
   UpdatePetriNetWithCDR(j, TEST_CDR)
-  // TODO: check that each set of rmt transitions is as expected
+
+  expected := map[int]petrinet.RemoteTransition {
+    ACT_TR: petrinet.RemoteTransition{
+      ID: ACT_TR,
+      InArcs: []petrinet.RemoteArc{},
+      OutArcs: []petrinet.RemoteArc{
+        petrinet.RemoteArc{PlaceID: PR_PLACE,Context: "j",Address: "",Weight: 1,Marks: 0},
+      },
+      InhibitorArcs: []petrinet.RemoteArc{},
+    },
+    DEACT_TR: petrinet.RemoteTransition{
+      ID: DEACT_TR,
+      InArcs: []petrinet.RemoteArc{
+        petrinet.RemoteArc{PlaceID: CTX_PLACE,Context: "j",Address: "",Weight: 1,Marks: 0},
+      },
+      OutArcs: []petrinet.RemoteArc{
+        petrinet.RemoteArc{PlaceID: PR_NOT_PLACE,Context: "j",Address: "",Weight: 1,Marks: 0},
+        petrinet.RemoteArc{PlaceID: CTX_PLACE,Context: "j",Address: "",Weight: 1,Marks: 0},
+      },
+      InhibitorArcs: []petrinet.RemoteArc{},
+    },
+  }
+  helperUpdatePetriNetWithCDRfunc(i, expected, t)
+
+  expected = make(map[int]petrinet.RemoteTransition)
+  helperUpdatePetriNetWithCDRfunc(j, expected, t)
 }
 
 func TestUpdateConflictSolverWithCDR(t *testing.T) {
