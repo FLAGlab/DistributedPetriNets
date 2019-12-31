20855774ce30677658914e42b63f346d354b5e79
jssosa10 <js.sosa10@uniandes.edu.co>
2019-12-30 21:59:03 -0500

    update serv

--- sleuth_server.go
+++ sleuth_server.go
@@ -19,8 +19,7 @@ func main() {
 	// In the real world, the Interface field of the sleuth.Config object
 	// should be set so that all services are on the same subnet.
 	config := &sleuth.Config{
-    Handler:  handler,
-    Interface: "eth0"
+    	Handler:  handler,
 		LogLevel: "debug",
 		Service:  "echo-service",
 	}
