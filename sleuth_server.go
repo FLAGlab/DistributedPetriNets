32a87f38b051accb6ef1bb313dc81897bee68d3b
ncardozo <n.cardozo@uniandes.edu.co>
2020-01-06 20:38:22 -0500

    no change
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

--- sleuth_server.go
+++ sleuth_server.go
@@ -19,7 +19,7 @@ func main() {
 	// In the real world, the Interface field of the sleuth.Config object
 	// should be set so that all services are on the same subnet.
 	config := &sleuth.Config{
-    	Handler:  handler,
+		Handler:  handler,
 		LogLevel: "debug",
 		Service:  "echo-service",
 	}
