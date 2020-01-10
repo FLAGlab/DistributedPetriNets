49f1616006224110694a79ad6147eb0bbcfa9cd5
Nicolas Cardozo <n.cardozo@uniandes.edu.co>
2020-01-10 18:38:24 -0500

    D

--- petrinet/transtion.go
+++ petrinet/transtion.go
@@ -56,19 +56,22 @@ func (t *Transition) Fire() error {
 		currArc.Place.Marks -= currArc.Weight
 		fmt.Printf("Fire 1!= %v \n", currArc.Place.Marks)
 	}
+	/*
 	for _, currArc := range t.OutArcs {
 		currArc.Place.Marks += currArc.Weight
 	}
+	*/
 	for _, remArc := range t.RemoteOutArcs {
 		fmt.Printf("voy a disparar, remoto \n")
 		valor = valor || remArc.fire()
 		fmt.Printf("Disparo remoto, o no? %v \n", valor)
 	}
+	/*
 	if !valor {
 		for _, currArc := range t.InArcs {
 			currArc.Place.Marks += currArc.Weight
 		}
-	}
+	}*/
 	return nil
 }
 
