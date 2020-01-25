adb1579bb9bc35de8cd2db6ea0adb9ae77c86ddf
jssosa10 <js.sosa10@uniandes.edu.co>
2020-01-25 10:23:18 -0500

    update ping

--- tests/ping/ping.go
+++ tests/ping/ping.go
@@ -8,10 +8,11 @@ import (
 
 func main() {
 	interf := os.Args[1]
-	fmt.Println("init Ping net....")
+	fmt.Printf("init Ping net.... %v\n", interf)
 	p := pn.InitPN(0)
 	p.AddTransition(1, 1)
-	p.AddPlace(1, 1, "ping")
+	p.AddPlace(1, "ping")
+	p.Places[1].AddMarks([]pn.Token{pn.Token{1}})
 	p.AddInArc(1, 1, 1)
 	p.AddRemoteOutArc(1, 1, "pong")
 	p.InitService(interf)
--- tests/ping/pong.go
+++ tests/ping/pong.go
@@ -3,14 +3,16 @@ package main
 import (
 	pn "github.com/FLAGlab/DistributedPetriNets/petrinet"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("init Pong net....")
+	interf := os.Args[1]
+	fmt.Printf("init Ping net.... %v\n", interf)
 	p := pn.InitPN(0)
 	p.AddTransition(1, 1)
-	p.AddPlace(1, 0, "pong")
+	p.AddPlace(1, "pong")
 	p.AddInArc(1, 1, 1)
 	p.AddRemoteOutArc(1, 1, "ping")
-	p.InitService()
+	p.InitService(interf)
 }
