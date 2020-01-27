33fe3ac76b23e20a20a5e41b21c5c990e8347d41
ncardozo <n.cardozo@uniandes.edu.co>
2020-01-26 19:27:31 -0500

    fixing ping to have the node name
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

--- tests/ping/ping.go
+++ tests/ping/ping.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
-	"os"
 	pn "github.com/FLAGlab/DistributedPetriNets/petrinet"
+	"os"
 )
 
 func main() {
 	interf := os.Args[1]
+	name := os.Args[2]
 	fmt.Printf("init Ping net.... %v\n", interf)
 	p := pn.InitPN(0)
 	p.AddTransition(1, 1)
-	p.AddPlace(1, "ping")
+	p.AddPlace(1, "ping", name)
 	p.Places[1].AddMarks([]pn.Token{pn.Token{1}})
 	p.AddInArc(1, 1, 1)
 	p.AddRemoteOutArc(1, 1, "pong")
--- tests/ping/pong.go
+++ tests/ping/pong.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	pn "github.com/FLAGlab/DistributedPetriNets/petrinet"
 	"fmt"
+	pn "github.com/FLAGlab/DistributedPetriNets/petrinet"
 	"os"
 )
 
 func main() {
 	interf := os.Args[1]
+	name := os.Args[2]
 	fmt.Printf("init Ping net.... %v\n", interf)
 	p := pn.InitPN(0)
 	p.AddTransition(1, 1)
-	p.AddPlace(1, "pong")
+	p.AddPlace(1, "pong", name)
 	p.AddInArc(1, 1, 1)
 	p.AddRemoteOutArc(1, 1, "ping")
 	p.InitService(interf)
