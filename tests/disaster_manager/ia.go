8bf9283edd9cee321843c95d7c6c777facfd52ff
Nicolas Cardozo <n.cardozo@uniandes.edu.co>
2020-01-27 17:21:09 -0500

    update ia
    changing the behavoir to have two mailboxes, one for police officers and one for firefighters

--- tests/disaster_manager/ia.go
+++ tests/disaster_manager/ia.go
@@ -11,11 +11,13 @@ func main() {
 	name := os.Args[2]
 	fmt.Printf("init SENSOR net.... %v\n", interf)
 	p := pn.InitPN(0)
-	p.AddTransition(1, 1) //assign
+	p.AddTransition(1, 1) //assign_police
+	p.AddTransition(2, 1) //assign_fire
 	p.AddPlace(1, "patients", name)
 	p.Places[1].AddMarks([]pn.Token{pn.Token{2}, pn.Token{1}, pn.Token{3}, pn.Token{4}, pn.Token{5}})
 	p.AddInArc(1, 1, 1)
+	p.AddInArc(1, 2, 1)
 	p.AddRemoteOutArc(1, 1, "police")
-	p.AddRemoteOutArc(1, 1, "fire")
+	p.AddRemoteOutArc(2, 1, "fire")
 	p.InitService(interf)
 }
