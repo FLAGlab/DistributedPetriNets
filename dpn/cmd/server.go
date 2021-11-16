3e3e54f1757c83665289130b6d8f27ebe39509ce
jssosa10 <juansesosaajedrez3@gmail.com>
2021-11-16 13:15:31 -0500

    server creation update

--- dpn/cmd/server.go
+++ dpn/cmd/server.go
@@ -12,7 +12,7 @@ func init() {
 var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "Run the server that calculate the reachability graph",
-	Long:  `Run the server that calculate the reachability graph`,
+	Long:  `Run the server that calculate the complete reachability graph`,
 	Run: func(cmd *cobra.Command, args []string) {
 		reachability.Run()
 	},
